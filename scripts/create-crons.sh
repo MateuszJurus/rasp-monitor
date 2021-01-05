cc17d4c510aa98f56e3f0f929c6ff48772c9117c
Mateusz Juruś <mateuszjur@gmail.com>
2021-01-05 02:25:48 +0100

    002: fixed cront script

--- scripts/create-crons.sh
+++ scripts/create-crons.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "sh $(pwd)/scripts/cron/tempcheck.sh" >> crontab;
+#temp cron
+crontab -l > temp_cron
+echo "*/1 * * * * sh $(pwd)/scripts/cron/tempcheck.sh" >> temp_cron
+#install new cron file
+crontab temp_cron
+rm temp_cron
