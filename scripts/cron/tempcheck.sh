9ff079a3923da6fd10a99149176ad6225c232ad0
Mateusz Juruś <mateuszjur@gmail.com>
2021-01-08 02:52:22 +0100

    002: passing path to temp script as an argument

--- scripts/cron/tempcheck.sh
+++ scripts/cron/tempcheck.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-temperature='/logs/temperature.log'
-#Check device temp with current date every minute
-echo $(date +"%d-%m-%Y %T"), `vcgencmd measure_temp` >> $PWD'/..'$temperature
+#TODO PASS A PATH INTO FUNCTION AS AN ARGUMENT
+check_temp () {
+    #Check device temp with current date every minute
+    echo $(date +"%d-%m-%Y %T"), `vcgencmd measure_temp` >> $1
+}
