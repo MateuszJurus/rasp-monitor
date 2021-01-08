94a6993a2e88cdc4748ab90d7e9969da488ad855
Mateusz Juruś <mateuszjur@gmail.com>
2021-01-09 00:54:52 +0100

    002: added variable for passing path to cron scripts

--- scripts/cron/tempcheck.sh
+++ scripts/cron/tempcheck.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-#TODO PASS A PATH INTO FUNCTION AS AN ARGUMENT
-check_temp () {
-    #Check device temp with current date every minute
-    echo $(date +"%d-%m-%Y %T"), `vcgencmd measure_temp` >> $1
-}
+#Check device temp with current date every minute
+echo $(date +"%d-%m-%Y %T"), `vcgencmd measure_temp` >> $1
