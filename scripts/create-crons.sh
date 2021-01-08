39f00047180be2ea065c00f9d5467f45761d0bdc
Mateusz Juruś <mateuszjur@gmail.com>
2021-01-08 01:20:03 +0100

    002: error check in cron script

--- scripts/create-crons.sh
+++ scripts/create-crons.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 #temp cron
-if crontab -l > temp_cron; then
+crontab -l > temp_cron
+if [ $? -eq 0 ]; then
     #install new cron file
     echo "*/1 * * * * sh $(pwd)/scripts/cron/tempcheck.sh" >> temp_cron
     crontab temp_cron
     rm temp_cron
-    echo -e "\033[0;36m Cron jobs added"
-else 
-    echo -e "\033[0;31m Error: Could not add cron jobs"
+    echo -e "\033[0;32m Cron jobs added successfuly"
+else
+    rm temp_cron
+    echo -e "\033[0;31m Error: cron jobs could not be added"
 fi
-
