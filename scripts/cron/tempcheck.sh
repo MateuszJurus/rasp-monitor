d0bd10b84da85397d71098b009776260ff4bd92c
Mateusz Juruś <mateuszjur@gmail.com>
2021-01-08 02:36:17 +0100

    002: fix temperature script paths

--- scripts/cron/tempcheck.sh
+++ scripts/cron/tempcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-temperature='logs/temperature.log'
+temperature='/logs/temperature.log'
 #Check device temp with current date
-echo $(date +"%d-%m-%Y %T"), `vcgencmd measure_temp` >> $PWD$temperature
+echo $(date +"%d-%m-%Y %T"), `vcgencmd measure_temp` >> $PWD'/../'$temperature
