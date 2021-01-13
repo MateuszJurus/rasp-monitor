1b3a9e726003d4f17d7d14361691695f0d662e45
Mateusz Juruś <mateuszjur@gmail.com>
2021-01-13 17:57:05 +0100

    004: polished displaying points on chart

--- src/js/heatChart.js
+++ src/js/heatChart.js
@@ -9,23 +9,21 @@ class Chart {
         this.elem = elem;
         this.dimensions = dimensions;
     }
-    //Parse date format into readable value [expected ex. 04-01-2021 22:53:01]
-    dateIntoValue() {
 
-    }
     //Parse temperature format into readable value [expected ex. 10,5'] 
     tempIntoValue(temp) {
         return temp.slice(0,-1);
     }
 
     //Create a point which will be displayed on the chart
-    createChartPoint(values) {
-        console.log(values)
+    createChartPoint(values, n, i) {
         const point = document.createElement('span');
         const y = this.tempIntoValue(values[1]);
         point.classList.add('chartPoint');
-        point.style.left = "1px";
-        point.style.bottom = y + "px";
+        //calculate how far apart points should be
+        let x = (this.elem.offsetWidth / (n-1))-1;
+        point.style.left = x*i + "px";
+        point.style.bottom = y*100-4600 + "px";
         this.elem.appendChild(point);
     }
 }
@@ -33,7 +31,7 @@ class Chart {
 getLogData().then(data => {
     heatChart = new Chart(data,chart);
     for(let i = 0; i < Object.keys(data).length; i++) {
-        heatChart.createChartPoint(data[i]);
+        heatChart.createChartPoint(data[i],Object.keys(data).length, i);
     }
 })
 
